8afe1a7777a556d3a6a881de7aa8900c7bf9bbdb
ICE0208 <doky3307@naver.com>
2024-02-18 21:33:54 +0900

    닉네임을 UUID로 변환 후, 해당 UUID가 존재하는지 확인

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,39 @@
 import Head from "next/head";
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import * as S from "@/styles/index.styled";
 
 export default function Home() {
+  const [inputNick, setInputNick] = useState<string>("");
+  const [convertedUUID, setConvertedUUID] = useState<string | null>(null);
+  const [status, setStatus] = useState<boolean | null>(null);
+
   const [uuidList, setUuidList] = useState<string[]>([]);
 
   const getUUIDs = async () => {
     const response = await fetch("/api/getuuids");
     const json = await response.json();
     setUuidList(json.data);
+  };
+
+  const nick2uuid = async (nick: string): Promise<string> => {
+    const response = await fetch(`/api/nick2uuid?&nick=${nick}`);
+    const json = await response.json();
+    const uuid = json.data;
+    return uuid;
+  };
+
+  const handleSearchBtn = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setConvertedUUID(null);
+    setStatus(null);
+    const uuid = await nick2uuid(inputNick);
+    console.log(uuid);
+    setConvertedUUID(uuid);
+    if (uuid === "NULL" || uuid === "ERROR") {
+      setStatus(null);
+    } else {
+      setStatus(uuidList.indexOf(uuid) !== -1);
+    }
   };
 
   useEffect(() => {
@@ -33,6 +58,26 @@ export default function Home() {
         />
       </Head>
       <S.Main>
+        <S.SearchWrapper>
+          <S.SearchTitle>닉네임으로 검색하기</S.SearchTitle>
+          <S.SearchForm onSubmit={handleSearchBtn}>
+            <S.SearchInput
+              onChange={(event) => setInputNick(event.target.value)}
+            />
+            <S.SearchBtn>검색</S.SearchBtn>
+          </S.SearchForm>
+          <S.SearchResultWrapper>
+            <S.SearchResultText1>{`UUID: ${
+              convertedUUID ?? "..."
+            }`}</S.SearchResultText1>
+            <S.SearchResultText2>
+              {`등록 상태: ${status === null ? "..." : status ? "O" : "X"}`}
+            </S.SearchResultText2>
+          </S.SearchResultWrapper>
+        </S.SearchWrapper>
+
+        <S.Bar></S.Bar>
+
         <S.ListWrapper>
           <S.ListTitle>{`등록된 UUID 리스트 (총 ${uuidList.length}명)`}</S.ListTitle>
           <S.ListUL>
--- src/styles/index.styled.ts
+++ src/styles/index.styled.ts
@@ -3,8 +3,39 @@ import styled from "styled-components";
 export const Main = styled.main`
   min-height: 100vh;
   display: flex;
-  justify-content: center;
+  align-items: center;
+  flex-direction: column;
   padding: 40px;
+`;
+
+export const SearchWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+export const SearchTitle = styled.h3`
+  font-size: 22px;
+  margin-bottom: 10px;
+`;
+export const SearchForm = styled.form`
+  margin-bottom: 6px;
+`;
+export const SearchInput = styled.input``;
+export const SearchBtn = styled.button``;
+export const SearchResultWrapper = styled.div`
+  min-width: 330px;
+  padding: 20px 10px;
+`;
+export const SearchResultText1 = styled.p`
+  margin-bottom: 8px;
+`;
+export const SearchResultText2 = styled.p``;
+
+export const Bar = styled.div`
+  width: 400px;
+  height: 0px;
+  border-bottom: 1px solid white;
+  margin: 20px 0;
 `;
 
 export const ListWrapper = styled.div`
