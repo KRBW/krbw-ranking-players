171fcb2adbc6cb6e2e68a98dd447e4edc1ce913d
ICE0208 <doky3307@naver.com>
2024-02-18 20:20:25 +0900

    불러온 UUID 화면에 뿌리기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import { useEffect, useState } from "react";
+import styled from "styled-components";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -34,7 +35,38 @@ export default function Home() {
           href="/favicon.ico"
         />
       </Head>
-      <main className={`${inter.className}`}></main>
+      <Main className={`${inter.className}`}>
+        <ListWrapper>
+          <ListTitle>{`등록된 UUID 리스트 (총 ${uuidList.length}명)`}</ListTitle>
+          <ListUL>
+            {uuidList.map((uuid) => {
+              return <ListLI key={uuid}>{uuid}</ListLI>;
+            })}
+          </ListUL>
+        </ListWrapper>
+      </Main>
     </>
   );
 }
+
+const Main = styled.main`
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  padding: 40px;
+`;
+
+const ListWrapper = styled.div`
+  min-width: 380px;
+`;
+const ListTitle = styled.h3`
+  text-align: center;
+  font-size: 26px;
+  margin-bottom: 14px;
+`;
+const ListUL = styled.ul``;
+const ListLI = styled.li`
+  border: 1px solid whitesmoke;
+  padding: 10px 8px;
+  text-align: center;
+`;
